583522f0670c5875ad42adfb880f146d35339602
Thanh Thu Cao <thuctse63589@fpt.edu.vn>
2021-10-10 23:15:33 +0700

    wip logo

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,15 +1,20 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { AppBar, Toolbar } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 import Logo from './Logo';
 
-const MainNavbar = (props) => (
-  <AppBar elevation={0} {...props}>
-    <Toolbar sx={{ height: 64 }}>
-      <RouterLink to="/">
-        <Logo />
-      </RouterLink>
-    </Toolbar>
-  </AppBar>
-);
-
+const MainNavbar = (props) => {
+  const account = useSelector((state) => state.account);
+  const { isLoggedIn } = account;
+  console.log(isLoggedIn);
+  return (
+    <AppBar elevation={0} {...props}>
+      <Toolbar sx={{ height: 64 }}>
+        <RouterLink to="/">
+          <Logo width="52px" height="52px" hidden={isLoggedIn ? 'false' : 'true'} />
+        </RouterLink>
+      </Toolbar>
+    </AppBar>
+  );
+};
 export default MainNavbar;
