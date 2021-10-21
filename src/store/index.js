9a6c1245c2ce5c8a8a3a924ec8f541e49288f0c1
chuqdu <quangdu2601@gmail.com>
2021-10-21 15:45:29 +0700

    Add reducer to store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import accountSlice from './account-slice';
 import studentSlice from './student-slice';
+import companySlice from './company-slice';
 
 const store = configureStore({
   reducer: {
     account: accountSlice.reducer,
-    students: studentSlice.reducer
+    students: studentSlice.reducer,
+    companies: companySlice.reducer
   }
 });
 
