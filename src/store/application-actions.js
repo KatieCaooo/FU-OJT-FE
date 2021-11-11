6c6af708e602b71670db984172c9678d8952dce1
chuqdu <quangdu2601@gmail.com>
2021-11-11 11:11:06 +0700

    Added actions

--- src/store/application-actions.js
+++ src/store/application-actions.js
@@ -42,11 +42,119 @@ export const fetchEvaluationData = (token, pageNo, pageSize, sortBy, search) => 
 };
 
 export const updateApplication = (token, application, pageNo, pageSize, sortBy, search) => async (dispatch) => {
+  const url = `${BASE_URL}/applications/${application.id}`;
+  const postData = async () => {
+    const response = await axios.put(
+      url,
+      {experience: application.experience, jobId: application.job.id, accountId: application.student.id, companyAccepted: application.companyAccepted === 'Accepted', studentConfirmed: application.studentConfirmed === 'Accepted'},
+      {
+        headers: getRequiredAuthenHeader(token)
+      }
+    );
 
+    if (response.status !== 200) {
+      throw new Error('Could not update application');
+    }
+
+    return response.data;
+  };
+
+  try {
+    await postData().then(async () => {
+      const fetchData = async () => {
+        const fetchUrl = `${BASE_URL}/applications`;
+        const response = await axios.get(fetchUrl, {
+          params: {
+            search: `id > 0${search && search !== '' ? `;${search}` : ''}`,
+            pageSize,
+            pageNo,
+            sortBy
+          },
+          headers: getRequiredAuthenHeader(token)
+        });
+
+        if (response.status !== 200) {
+          throw new Error('Could not fetch data');
+        }
+
+        return response.data;
+      };
+
+      const response = await fetchData();
+      let applications = response.data;
+      applications = applications.map((application) => ({
+        id: application.id, studentCode: application.student.studentCode, major: application.majors.name,
+        experience: application.experience, job: application.job.name, company: application.company.name,
+        companyAccepted: application.companyAccepted, studentConfirmed: application.studentConfirmed
+      }));
+      dispatch(
+        applicationActions.replaceApplicationList({
+          applications: applications || [],
+          totalQuantity: response.totalElements
+        })
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const createApplication = (token, application, pageNo, pageSize, sortBy, search) => async (dispatch) => {
-  
+  const url = `${BASE_URL}/applications`;
+  const postData = async () => {
+    const response = await axios.post(
+      url,
+      {experience: application.experience, jobId: application.job.id, accountId: application.student.id},
+      {
+        headers: getRequiredAuthenHeader(token)
+      }
+    );
+
+    if (response.status !== 200) {
+      throw new Error('Could not create application');
+    }
+
+    return response.data;
+  };
+
+  try {
+    await postData().then(async () => {
+      const fetchData = async () => {
+        const fetchUrl = `${BASE_URL}/applications`;
+        const response = await axios.get(fetchUrl, {
+          params: {
+            search: `id > 0${search && search !== '' ? `;${search}` : ''}`,
+            pageSize,
+            pageNo,
+            sortBy
+          },
+          headers: getRequiredAuthenHeader(token)
+        });
+
+        if (response.status !== 200) {
+          throw new Error('Could not fetch data');
+        }
+
+        return response.data;
+      };
+
+      const response = await fetchData();
+      let applications = response.data;
+      applications = applications.map((application) => ({
+        id: application.id, studentCode: application.student.studentCode, major: application.majors.name,
+        experience: application.experience, job: application.job.name, company: application.company.name,
+        companyAccepted: application.companyAccepted, studentConfirmed: application.studentConfirmed
+      }));
+      dispatch(
+        applicationActions.replaceApplicationList({
+          applications: applications || [],
+          totalQuantity: response.totalElements
+        })
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const deleteApplication = (token, application, pageNo, pageSize, sortBy, search) => async (dispatch) => {
@@ -67,38 +175,40 @@ export const deleteApplication = (token, application, pageNo, pageSize, sortBy, 
   };
 
   try {
-    const url = `${BASE_URL}/applications`;
-    const fetchData = async () => {
-      const response = await axios.get(url, {
-        params: {
-          search: `id > 0${search && search !== '' ? `;${search}` : ''}`,
-          pageSize,
-          pageNo,
-          sortBy
-        },
-        headers: getRequiredAuthenHeader(token)
-      });
+    await postData().then(async () => {
+      const fetchData = async () => {
+        const fetchUrl = `${BASE_URL}/applications`;
+        const response = await axios.get(fetchUrl, {
+          params: {
+            search: `id > 0${search && search !== '' ? `;${search}` : ''}`,
+            pageSize,
+            pageNo,
+            sortBy
+          },
+          headers: getRequiredAuthenHeader(token)
+        });
 
-      if (response.status !== 200) {
-        throw new Error('Could not fetch data');
-      }
+        if (response.status !== 200) {
+          throw new Error('Could not fetch data');
+        }
 
-      return response.data;
-    };
+        return response.data;
+      };
 
-    const response = await fetchData();
-    let applications = response.data;
-    applications = applications.map((application) => ({
-      id: application.id, studentCode: application.student.studentCode, major: application.majors.name,
-      experience: application.experience, job: application.job.name, company: application.company.name,
-      companyAccepted: application.companyAccepted, studentConfirmed: application.studentConfirmed
-    }));
-    dispatch(
-      applicationActions.replaceApplicationList({
-        applications: applications || [],
-        totalQuantity: response.totalElements
-      })
-    );
+      const response = await fetchData();
+      let applications = response.data;
+      applications = applications.map((application) => ({
+        id: application.id, studentCode: application.student.studentCode, major: application.majors.name,
+        experience: application.experience, job: application.job.name, company: application.company.name,
+        companyAccepted: application.companyAccepted, studentConfirmed: application.studentConfirmed
+      }));
+      dispatch(
+        applicationActions.replaceApplicationList({
+          applications: applications || [],
+          totalQuantity: response.totalElements
+        })
+      );
+    });
   } catch (error) {
     console.log(error);
   }
